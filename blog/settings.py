7d180f1a33578ae362767580be467b284e38d909
1 <1@qq.com>
2025-07-12 19:54:44 +0800

    静态资源文件

--- blog/settings.py
+++ blog/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+#设置静态资源路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

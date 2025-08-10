6b7b7aa2fe0fcdbb182434f2c9d5596ef8f820b4
1 <1@qq.com>
2025-08-10 11:01:29 +0800

    博客保存

--- users/views.py
+++ users/views.py
@@ -411,7 +411,7 @@ class UserCenterView(LoginRequiredMixin, View):
         # 5.返回响应
         return response
 
-from home.models import ArticleCategory
+from home.models import ArticleCategory,Article
 class WriteBlogView(LoginRequiredMixin,View):
 
     def get(self,request):
@@ -423,4 +423,48 @@ class WriteBlogView(LoginRequiredMixin,View):
         return render(request,'write_blog.html',context)
 
 
+    def post(self,request):
+        """
+        1.接收数据
+        2.验证数据
+        3.数据入库
+        4.跳转到指定页面（暂时首页）
+        """
+        # 1.接收数据
+        avatar=request.FILES.get('avatar')
+        title=request.POST.get('title')
+        category_id=request.POST.get('category')
+        tags=request.POST.get('tags')
+        sumary=request.POST.get('sumary')
+        content=request.POST.get('content')
+        user=request.user
 
+        # 2.验证数据
+        # 2.1 验证参数是否齐全
+        if not all([avatar,title,category_id,sumary,content]):
+            return HttpResponseBadRequest('参数不全')
+        # 2.2 判断文章分类id数据是否正确
+        try:
+            article_category=ArticleCategory.objects.get(id=category_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseBadRequest('没有此分类信息')
+
+        # 3. 保存到数据库
+        try:
+            article=Article.objects.create(
+                author=user,
+                avatar=avatar,
+                category=article_category,
+                tags=tags,
+                title=title,
+                sumary=sumary,
+                content=content
+            )
+        except Exception as e:
+            logger.error(e)
+            return HttpResponseBadRequest('发布失败，请稍后再试')
+
+
+        # 4.暂时先跳转到首页
+        # 返回响应，跳转到文章详情页面
+        return redirect(reverse('home:index'))

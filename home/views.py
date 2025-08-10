ce6cdbad1c14beb712fb8927cf7ec3f326e497b8
1 <1@qq.com>
2025-08-10 11:21:38 +0800

    首页分类数据展示

--- home/views.py
+++ home/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.views import View
-
+from home.models import ArticleCategory
+from django.http import HttpResponseNotFound
 # Create your views here.
 
 class IndexView(View):
-    """首页广告"""
-
+    """
+    首页广告
+    1.获取所有分类信息
+    2.接收用户点击的分类id
+    3.根据分类id进行分类的查询
+    4.组织数据传递给模板
+    """
     def get(self, request):
         """提供首页广告界面"""
-        return render(request, 'index.html')
+        # 1.获所有分类信息
+        categories = ArticleCategory.objects.all()
+        # 2.接收用户点击的分类id
+        # ?cat_id=xxx&page_num=xxx&page_size=xxx
+        cat_id = request.GET.get('cat_id', 1)
+        # 3.根据分类1d进行分类的查诡
+        try:
+            category = ArticleCategory.objects.get(id=cat_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseNotFound('没有此分类')
+        # 4.组织数据传递给模板
+        context = {
+            'categories':categories,
+            'category':category
+        }
+        # return render(request, 'index.html')
+        return render(request, 'index.html', context=context)
